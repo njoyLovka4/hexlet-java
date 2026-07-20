8961665a72f0c36a360a191b364b6f0275f6ac16
Alexander <njoy.lovka4@gmail.com>
2026-07-21 01:27:32 +0300

    add librory + замена текста One.java

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,15 @@
 package io.hexlet;
 
+// Импортируем класс для работы со строками из внешней библиотеки
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        // Переворачиваем строку "Hexlet"
+        String reversed = StringUtils.reverse("Hexlet");
+        
+        // Выводим результат на экран
+        System.out.println(reversed);
     }
 }
+
